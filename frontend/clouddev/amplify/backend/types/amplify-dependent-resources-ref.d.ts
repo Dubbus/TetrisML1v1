30699193b6b0f2fb54e714e40de9f678341d5efc
DmitryVolf <volf.dmitry@gmail.com>
2025-10-25 14:06:23 -0400

    Created cloud development for frontend

---
+++ frontend/clouddev/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "tetrismldev": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
